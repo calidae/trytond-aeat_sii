af7273af1be7d8e2afdb11544ba7f2b6df4415fa
?ngel ?lvarez <angel@nan-tic.com>
2017-06-07 11:00:10 +0200

    add sii key on taxes and method to load on invoices

--- __init__.py
+++ __init__.py
@@ -7,18 +7,19 @@ from . import aeat
 from . import party
 from . import company
 from . import load_pkcs12
+from . import account
 
 
 def register():
     Pool.register(
+        account.TemplateTax,
+        account.Tax,
         party.Party,
         company.Company,
         invoice.Invoice,
         load_pkcs12.LoadPKCS12Start,
         aeat.SIIReport,
         aeat.SIIReportLine,
-        aeat.IssuedTrytonInvoiceMapper,
-        aeat.RecievedTrytonInvoiceMapper,
         module='aeat_sii', type_='model')
     Pool.register(
         load_pkcs12.LoadPKCS12,
