834c5c18a1c4372600b0745ee36d3cfef710aed8
Raimon Esteve <raimon@nan-tic.com>
2017-06-29 10:18:44 +0200

    Add fill keys on invoice purchase Since changeset-087f67664279 add extra depends in sale, do the same for purchase

--- invoice.py
+++ invoice.py
@@ -13,32 +13,12 @@ from .aeat import (
     EXCEMPTION_CAUSE, INTRACOMUNITARY_TYPE, COMMUNICATION_TYPE)
 
 
-__all__ = ['Invoice', 'Sale']
+__all__ = ['Invoice', 'Sale', 'Purchase']
 
 
 _SII_INVOICE_KEYS = ['sii_book_key', 'sii_issued_key', 'sii_received_key',
         'sii_subjected_key', 'sii_excemption_key',
         'sii_intracomunity_key']
-
-
-class Sale:
-    __metaclass__ = PoolMeta
-    __name__ = 'sale.sale'
-
-    def create_invoice(self, invoice_type):
-        invoice = super(Sale, self).create_invoice(invoice_type)
-
-        if not invoice:
-            return invoice
-
-        tax = invoice.taxes and invoice.taxes[0]
-        if not tax:
-            return invoice
-        for field in _SII_INVOICE_KEYS:
-            setattr(invoice, field, getattr(tax.tax, field))
-
-        invoice.save()
-        return invoice
 
 
 class Invoice:
@@ -191,3 +171,43 @@ class Invoice:
         default = default.copy()
         default['sii_records'] = None
         return super(Invoice, cls).copy(records, default=default)
+
+
+class Sale:
+    __metaclass__ = PoolMeta
+    __name__ = 'sale.sale'
+
+    def create_invoice(self, invoice_type):
+        invoice = super(Sale, self).create_invoice(invoice_type)
+        if not invoice:
+            return
+
+        tax = invoice.taxes and invoice.taxes[0]
+        if not tax:
+            return invoice
+
+        for field in _SII_INVOICE_KEYS:
+            setattr(invoice, field, getattr(tax.tax, field))
+        invoice.save()
+
+        return invoice
+
+
+class Purchase:
+    __metaclass__ = PoolMeta
+    __name__ = 'purchase.purchase'
+
+    def create_invoice(self, invoice_type):
+        invoice = super(Purchase, self).create_invoice(invoice_type)
+        if not invoice:
+            return
+
+        tax = invoice.taxes and invoice.taxes[0]
+        if not tax:
+            return invoice
+
+        for field in _SII_INVOICE_KEYS:
+            setattr(invoice, field, getattr(tax.tax, field))
+        invoice.save()
+
+        return invoice
