1fabcc7523b57a4672a296e546bd161accdb9d86
Bernat Brunet Torruella <bernat@nan-tic.com>
2018-07-08 23:01:54 +0200

    Fix problem when set sii key from invoice tax information

--- invoice.py
+++ invoice.py
@@ -166,7 +166,11 @@ class Invoice:
         return res
 
     def _set_sii_keys(self):
-        tax = self.taxes and self.taxes[0]
+        tax = None
+        for t in self.taxes:
+            if t.tax_used:
+                tax = t
+                break
         if not tax:
             return
         for field in _SII_INVOICE_KEYS:
