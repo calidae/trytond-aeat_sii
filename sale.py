acf0828df707a605bd37d7baa997773b71d58957
Bernat Brunet Torruella <bernat@nan-tic.com>
2019-07-11 17:16:20 +0200

    Fix operation key set when create invoice from sales. Fix translations and add new infomration. Fix operation key set when create credit invoice from invoice.

---
+++ sale.py
@@ -0,0 +1,23 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from decimal import Decimal
+from trytond.pool import PoolMeta
+
+__all__ = ['Sale']
+__metaclass__ = PoolMeta
+
+ZERO = Decimal('0.0')
+
+
+class Sale:
+    'Sale'
+    __name__ = 'sale.sale'
+
+    def create_invoice(self):
+        invoice = super(Sale, self).create_invoice()
+        if invoice.untaxed_amount < ZERO:
+            invoice.sii_operation_key = 'R1'
+        else:
+            invoice.sii_operation_key = 'F1'
+
+        return invoice
