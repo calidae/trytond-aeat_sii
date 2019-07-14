a6b94e4f259aa45a08f92c895c7a5ffaf82c43ab
Bernat Brunet Torruella <bernat@nan-tic.com>
2019-07-14 17:25:33 +0200

    Update some thing for python3

--- tests/test_aeat_sii.py
+++ tests/test_aeat_sii.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
@@ -6,11 +7,24 @@ import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_teardown
 from trytond.tests.test_tryton import doctest_checker
+from trytond.modules.aeat_sii.tools import unaccent
 
 
 class AeatSIITestCase(ModuleTestCase):
     'Test AEAT SII module'
     module = 'aeat_sii'
+
+    def test_unaccent(self):
+        for value, result in [
+                ('aeiou', 'aeiou'),
+                ('áéíóú', 'aeiou'),
+                ('__aéiou__', 'aeiou'),
+                ('__aé@ou__', 'ae_ou'),
+                (u'aeiou', 'aeiou'),
+                (u'áéíóú', 'aeiou'),
+                (b'aeiou', 'aeiou'),
+                ]:
+            self.assertEqual(unaccent(value), result)
 
 
 def suite():
