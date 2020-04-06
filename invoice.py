45435d80818a588868c711be0ac77725f3839bfe
Bernat Brunet <bernat@nan-tic.com>
2020-04-06 11:58:45 +0200

    Fix bug when control if create sii lines from invoice post, if the move are draft, because the in invoice when are valdiated crete move in draft state

--- invoice.py
+++ invoice.py
@@ -467,7 +467,7 @@ class Invoice(metaclass=PoolMeta):
 
         invoices2checksii = []
         for invoice in invoices:
-            if not invoice.move:
+            if not invoice.move or invoice.move.state == 'draft':
                 invoices2checksii.append(invoice)
 
         super(Invoice, cls).post(invoices)
